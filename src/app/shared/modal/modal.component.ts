5327f894f2242992e3b8c1b540c1e4d7fd504ccd
Arthur Ruffatto <arthurruffatto@hotmail.com>
2024-06-13 17:29:49 +0200

    added elementref to move the modal to the body, so the css doesn't affect it

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, ElementRef, Input, OnInit} from '@angular/core';
 import {ModalService} from "../../services/modal.service";
 
 @Component({
@@ -9,10 +9,12 @@ import {ModalService} from "../../services/modal.service";
 export class ModalComponent implements OnInit{
   @Input() modalID = ''
 
-  constructor(public modal: ModalService) {
+  constructor(public modal: ModalService, public el: ElementRef) {
   }
 
   ngOnInit() {
+    //this is moving the app-auth-modal from the app.component to the body
+    document.body.appendChild(this.el.nativeElement)
   }
 
   closeModal() {
