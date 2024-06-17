9e9d792284aa7d6e59a0122156a404357b49c62e
Arthur Ruffatto <arthurruffatto@hotmail.com>
2024-06-17 16:06:59 +0200

    added tab behaviors

--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -10,5 +10,25 @@ export class TabsContainerComponent implements AfterContentInit {
   @ContentChildren(TabComponent) tabs: QueryList<TabComponent> = new QueryList();
 
   ngAfterContentInit() {
+    const activeTab = this.tabs.filter(tab => tab.active);
+
+    if (!activeTab || activeTab.length === 0) {
+      this.selectTab(this.tabs.first);
+    }
+  }
+
+  selectTab(tab: TabComponent) {
+    this.tabs.forEach(tab => tab.active = false);
+
+    tab.active = true;
+
+    return false; //this return is to prevent the default behavior of the component
+  }
+
+  getTabClass(tab: TabComponent) {
+    return {
+      'hover:hover:text-indigo-400' : !tab.active,
+      'hover:text-white bg-indigo-400' : tab.active
+    }
   }
 }
