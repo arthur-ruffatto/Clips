f1e4f71ed7eb92517398afe4fc5aee722262848b
Arthur Ruffatto <arthurruffatto@hotmail.com>
2024-06-20 16:52:16 +0200

    created separate component for inputs and added validation for every register input

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormGroup, FormControl, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,48 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  name = new FormControl('', [
+    Validators.required,
+    Validators.minLength(2)
+  ])
+  email = new FormControl('', [
+    Validators.required,
+    Validators.email
+  ])
+  age = new FormControl('', [
+    Validators.required,
+    Validators.min(18),
+    Validators.max(120)
+  ])
+  password = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)
+  ])
+  confirmPassword = new FormControl('', [
+    Validators.required,
+  ])
+  phoneNumber = new FormControl('', [
+    Validators.required,
+    Validators.minLength(15),
+    Validators.maxLength(15)
+  ])
 
+  registerForm = new FormGroup({
+    name: this.name,
+    email: this.email,
+    age: this.age,
+    password: this.password,
+    confirmPassword: this.confirmPassword,
+    phoneNumber: this.phoneNumber,
+  })
+
+  alert = false;
+  alertMsg = 'Please wait while your account is being created.';
+  color = 'blue';
+
+  register() {
+    this.alert = true;
+    this.alertMsg = 'Please wait while your account is being created.';
+    this.color = 'blue';
+  }
 }
