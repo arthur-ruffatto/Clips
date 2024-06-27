bfc479d95268af3df3284a40c1b4b618feb1e0a2
Arthur Ruffatto <arthurruffatto@hotmail.com>
2024-06-27 15:49:37 +0200

    firebase setup

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBuzG_P_k2_gpku_Izpg5pH_B2RFf-gmUw",
+    authDomain: "clips-37c83.firebaseapp.com",
+    projectId: "clips-37c83",
+    storageBucket: "clips-37c83.appspot.com",
+    //messagingSenderId: "40184583212",
+    appId: "1:40184583212:web:8d2b7fb30decaddfae2d89"
+  }
+};
