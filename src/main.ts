4901cf20f631cff01af60680cb2c4490383fd579
Arthur Ruffatto <arthurruffatto@hotmail.com>
2024-07-01 14:53:42 +0200

    initialize firebase first

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
 
 import { AppModule } from './app/app.module';
+import {environment} from "./environments/environment.development";
 
-platformBrowserDynamic().bootstrapModule(AppModule, {
-  ngZoneEventCoalescing: true
+firebase.initializeApp(environment.firebase)
+
+let appInit = false
+
+firebase.auth().onAuthStateChanged(user => {
+  if(!appInit){
+    platformBrowserDynamic().bootstrapModule(AppModule, {
+      ngZoneEventCoalescing: true
+    })
+      .catch(err => console.error(err));
+  }
 })
-  .catch(err => console.error(err));
+
+
